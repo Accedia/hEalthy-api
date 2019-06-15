700d8f27abb1336b25ddfd50971454ecaceeb418
Nikolay Nikolov <nikolay.ts.nikolov@accedia.com>
2019-06-15 18:31:47 +0300

    query substances endpoint init

---
+++ src/ProcessSubstancesRequest.ts
@@ -0,0 +1,3 @@
+export class ProcessSubstancesRequest {
+    query: string;
+}
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { ConfigService } from './config/config.service';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { join } from 'path';
 import * as cors from 'cors';
 
@@ -10,15 +9,6 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cors());
   app.useStaticAssets(join(__dirname, '..', 'public'));
-  const options = new DocumentBuilder()
-    .setTitle('Startup')
-    .setDescription('Startup project with authentication')
-    .setVersion('1.0')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
-
   await app.listen(app.get(ConfigService).port);
 }
 bootstrap();
